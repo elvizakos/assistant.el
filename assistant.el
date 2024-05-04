dff2f02f38fd3e304b86e7ac538ef29202f10045
elvizakos <elvizakos@yahoo.gr>
2024-05-04 13:29:27 +0300

    Build basic minor-mode structure

--- assistant.el
+++ assistant.el
@@ -1,57 +1,133 @@
-(require 'request)
+;;; assistant.el --- Minor mode for getting help and chat with ollama and other AI.
+;;; -*- coding: utf-8 -*-
 
-;; curl -X POST http://192.168.68.8:11435/api/generate -d '{
-;; "model" : "yi",
-;; "prompt" : "Why is the sky blue?"
-;; }'
+;; Copyright © 2024, Nikos Skarmoutsos
 
-(setq
- assistant-server-url "http://192.168.68.8:11434/api/generate"
- ;; assistant-server-url "http://lvzcms.debian.home.lan/tests/requests?f=text"
- assistant-model "qwen:0.5b"
- assistant-prompt "Why is the sky blue?"
- )
+;; Version: %%VERSION%%
+;; Author: Nikos Skarmoutsos <elvizakos AT yahoo DOT gr>
+;; Maintainer: Nikos Skarmoutsos
+;; Created: May 2024
+;; Keywords: string
+;; License: GNU General Public License >=2
+;; Distribution: This file is not part of Emacs
 
-(request
-  assistant-server-url
-  :type "POST"
+;;; Code:
 
-  ;; :params (list (cons "model" assistant-model)
-  ;; 				(cons "prompt" assistant-prompt))
-  ;; :data  (format "model=%s&prompt=%s" assistant-model assistant-prompt)
+;;---- CONSTANTS ------------------------------------------------------------------
+(defconst assistant/assistant-version "%%VERSION%%" "Assistant version.")
 
-  ;; :headers '(("Content-Type" . "application/json"))
+;;---- VARIABLES ------------------------------------------------------------------
+(defvar assistant/assistant-keymap (make-sparse-keymap) "Keymap for assistant.")
 
-  ;; :data  (json-encode `(("model" . ,assistant-model)
-  ;; 						("prompt" . ,assistant-prompt)))
-  ;; :parser 'json-read
+(defvar assistant/prompt "" "The user prompt.")
 
+(defvar assistant/response "" "The assistant's response.")
 
-  :data  (json-encode (list (cons "model" assistant-model)
-			   (cons "prompt" assistant-prompt)))
-  :headers '(("Accept" . "application/json")
-			 ("Content-Type" . "application/json")
-			 )
+(defvar assistant/$buffer nil "The assistant's buffer.")
 
-  :success (cl-function
-			(lambda (&key data &allow-other-keys)
-			  ;;(message ">> %S" (assoc-default 'form data))
-			  ;; (message "%s" data)
-			  (with-current-buffer (get-buffer-create "**TESTS**")
-				(erase-buffer)
-				(insert (format "%s" data))
-				)
-			  ))
+;;---- OPTIONS --------------------------------------------------------------------
+(defgroup assistant nil "Assistant minor mode settings."
+  :group 'tools)
 
-  :error (cl-function
-		  (lambda (&key error-thrown &allow-other-keys&rest _)
-			(message "Error: %S" error-thrown)
-			))
+(defcustom assistant/lighter " A" "Label of minor mode for the modeline."
+  :type 'string
+  :group 'assistant)
 
-  :complete (lambda (&rest _) (message "Finished!"))
+(defcustom assistant/server-url "http://192.168.68.8:11434/api/generate" "The URL of the ollama."
+  :type 'string
+  :group 'assistant)
 
-  :status-code '((400 . (lambda (&rest _) (message "Got 400")))
-				 (418 . (lambda (&rest _) (message "Got 418")))
-				 (200 . (lambda (&rest _) (message "Got 200")))
-				 )
-  )
+(defcustom assistant/chat-model "llama2:7b" "The model to be used for chat."
+  :type 'string
+  ;;:type '(choice (const :tag "codegemma:2b" "codegemma:7b" "codellama:7b" "gemma:2b" "gemma:7b" "llama2:7b" "llama2:latest" "llama2:text" "llama2-uncensored:7b" "orca-mini:latest" "phi3:latest" "qwen:0.5b" "qwen:1.8b" "starcoder2:3b" "starcoder2:7b" "starcoder2:latest" "tinydolphin:latest" "tinyllama:latest" "yi:latest"))
+  :group 'assistant)
+
+(defcustom assistant/coding-model "starcoder2:7b" "The model to be used for code completion."
+  :type 'string
+  ;;:type '(choice (const :tag "codegemma:2b" "codegemma:7b" "codellama:7b" "gemma:2b" "gemma:7b" "llama2:7b" "llama2:latest" "llama2:text" "llama2-uncensored:7b" "orca-mini:latest" "phi3:latest" "qwen:0.5b" "qwen:1.8b" "starcoder2:3b" "starcoder2:7b" "starcoder2:latest" "tinydolphin:latest" "tinyllama:latest" "yi:latest"))
+  :group 'assistant)
+;; '("codegemma:2b" "codegemma:7b" "codellama:7b" "gemma:2b" "gemma:7b" "llama2:7b" "llama2:latest" "llama2:text" "llama2-uncensored:7b" "orca-mini:latest" "phi3:latest" "qwen:0.5b" "qwen:1.8b" "starcoder2:3b" "starcoder2:7b" "starcoder2:latest" "tinydolphin:latest" "tinyllama:latest" "yi:latest")
+
+(defcustom assistant/buffer-name "** ASSISTANT **" "The name of the assistant's buffer."
+  :type 'string
+  :group 'assistant)
+
+;;---- FUNCTIONS ------------------------------------------------------------------
+
+(defun assistant/json-get-response ( str )
+  (let ((ar (split-string str "\n"))
+		(i 0)
+		(itm "")
+		(res "")
+		(done nil)
+		(ret "")
+		)
+	(while (< i (+ 1 (length ar)))
+	  (if (and (string= (type-of (nth i ar)) "string") (> (length (nth i ar)) 0) (string= (substring (nth i ar) 0 1) "{"))
+		  (setq itm (json-parse-string (nth i ar))
+				res (gethash "response" itm)
+				done (gethash "done" itm))
+		)
+	  (setq i (1+ i)
+			ret (concat ret res))
+	  )
+	ret))
+
+(defun assistant/request ( model prompt ) "Function to get response "
+	   (setq assistant/response ""
+			 assistant/$buffer (get-buffer-create assistant/buffer-name))
+	   ;; (set-buffer assistant/$buffer)
+	   (other-window 1 t)
+	   (switch-to-buffer assistant/$buffer)
+	   (with-current-buffer assistant/$buffer
+		 (erase-buffer)
+
+		 (request
+		   assistant/server-url
+		   :type "POST"
+
+		   :data  (json-encode (list (cons "model" model)
+									 (cons "prompt" prompt)))
+		   ;; :parser 'json-read
+
+		   :headers '(("Accept" . "application/json")
+					  ("Content-Type" . "application/json"))
+
+		   :success (cl-function
+					 (lambda (&key data &allow-other-keys)
+						 (insert (assistant/json-get-response data))))
+
+		   :error (cl-function
+				   (lambda (&key error-thrown &allow-other-keys&rest _)
+					 (error "Error: %S" error-thrown)
+					 ))
+
+		   :complete (lambda (&rest _) (message "Finished!"))
+
+		   ;; :status-code '((400 . (lambda (&rest _) (message "Got 400")))
+		   ;; 				(418 . (lambda (&rest _) (message "Got 418")))
+		   ;; 				(200 . (lambda (&rest _) (message "Got 200")))
+		   ;; 				)
+		   ))
+	   nil
+	   )
+
+;;---- MINOR MODE ------------------------------------------------------------------
+(define-minor-mode assistant-mode "Assistant minor mode."
+  :lighter assistant/lighter
+  :keymap (let ((assistantmap (make-sparse-keymap)))
+			(require 'request)
+			(require 'json)
+
+			(setq assistant/$buffer (get-buffer-create assistant/buffer-name))
+			;; (setq assistant/$buffer (get-buffer-create assistant/buffer-name))
+			
+
+			assistant/assistant-keymap)
+  :global 1
+
+  (make-local-variable 'assistant/assistant-keymap))
+
+(assistant-mode 1)
+
+(provide 'assistant-mode)
