7245d1175472f3e1412fe3ee75b2cbb5be48edfa
elvizakos <elvizakos@yahoo.gr>
2025-02-10 02:10:38 +0200

    Tests

--- assistant-mode.el
+++ assistant-mode.el
@@ -335,9 +335,12 @@
 	   (interactive)
 	   (let ((is-visible (get-buffer-window assistant/$buffer)))
 		 (if is-visible
-			 (register-to-window-configuration assistant/window-register)
+			 ;; (set-window-configuration assistant/window-register)
+			 ;;(register-to-window-configuration assistant/window-register)
+			 (jump-to-register assistant/window-register)
 		   (progn
 			 (window-configuration-to-register assistant/window-register)
+			 ;; (set-register assistant/window-register (list (current-window-configuration) nil))
 			 (delete-other-windows)
 			 (split-window-horizontally)
 			 (switch-to-buffer-other-window assistant/$buffer)
